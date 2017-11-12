9dbba9217e2a91f420798a883702f663ceb82bc1
kristal111 <levon.babayan_2013@mail.ru>
2017-11-12 23:18:49 +0400

    session bug

--- app/User.php
+++ app/User.php
@@ -8,22 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
+    protected $table="users";
+    protected $primaryKey = 'user_id';
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
 }
