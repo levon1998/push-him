31a281e08b89f7bcc48a4560a797d2cbc0e91f95
kristal111 <levon.babayan_2013@mail.ru>
2017-11-14 00:46:22 +0400

    fexed

--- app/User.php
+++ app/User.php
@@ -11,4 +11,16 @@ class User extends Authenticatable
     protected $table="users";
     protected $primaryKey = 'id';
 
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+    ];
 }
